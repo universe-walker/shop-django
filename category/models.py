18b9f06d5e250c0ce703a0e1569aa53f9ea6a935
Nikita <nikita.motlitch@yandex.ru>
2021-08-23 13:39:38 +0300

    Add verbose_name, str, Meta to models Category, Product

--- category/models.py
+++ category/models.py
@@ -5,37 +5,52 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Category(MPTTModel, models.Model):
-    name = models.CharField(_('Название'), max_length=100)
-    img = models.ImageField()
+    name = models.CharField(_('название'), max_length=100)
+    img = models.ImageField(_('путь к картинке'))
     slug = models.SlugField(max_length=100)
     parent = TreeForeignKey(
         'self',
         on_delete=models.CASCADE,
         blank=True,
         null=True,
-        related_name='children'
+        related_name='children',
+        verbose_name=_('родительская категория')
     )
 
     class Meta:
         ordering = ['tree_id', 'lft']
+        verbose_name = _('категория')
+        verbose_name_plural = _('категории')
 
     class MPTTMeta:
         order_insertion_by = ['name']
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=140)
-    description = models.TextField(max_length=255)
-    price = models.IntegerField(validators=[MinValueValidator(0)])
-    available = models.BooleanField(default=True)
-    available_count = models.IntegerField(validators=[MinValueValidator(0)])
+    name = models.CharField(_('название товара'), max_length=140)
+    description = models.TextField(_('описание'), max_length=255)
+    price = models.IntegerField(_('цена'), validators=[MinValueValidator(0)])
+    available = models.BooleanField(_('Доступен'), default=True)
+    available_count = models.IntegerField(_('Доступное количество'), validators=[MinValueValidator(0)])
     slug = models.SlugField(max_length=140)
-    rating = models.FloatField(validators=[MinValueValidator(0)])
+    rating = models.FloatField(_('Рейтинг'), validators=[MinValueValidator(0)])
     category = models.ForeignKey(
         'Category',
         on_delete=models.CASCADE,
-        related_name='products'
+        related_name='products',
+        verbose_name='категория'
     )
+
+    class Meta:
+        ordering = ['-price']
+        verbose_name = _('товар')
+        verbose_name_plural = _('товары')
+
+    def __str__(self):
+        return f'{self.name} категории: {self.category}'
 
 
 class Characteristic(models.Model):
