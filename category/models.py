bcfd7203ed0f32d56bd529b4b875e90421e34c5d
Nikita <nikita.motlitch@yandex.ru>
2021-08-23 11:21:27 +0300

    Add models Category and Product

--- category/models.py
+++ category/models.py
@@ -1,3 +1,28 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField()
+    slug = models.SlugField(max_length=100)
+    parent = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        related_name='childs'
+    )
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=140)
+    description = models.TextField(max_length=255)
+    price = models.IntegerField(validators=[MinValueValidator(0)])
+    available = models.BooleanField(default=True)
+    available_count = models.IntegerField(validators=[MinValueValidator(0)])
+    slug = models.SlugField(max_length=140)
+    rating = models.FloatField(validators=[MinValueValidator(0)])
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
