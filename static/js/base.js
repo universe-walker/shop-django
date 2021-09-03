9cedbf606615b604849ac530816ec0b2d0ce99a3
Nikita <nikita.motlitch@yandex.ru>
2021-09-03 22:34:17 +0300

    Add search advice: html, css, js

---
+++ static/js/base.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+  $(".search__advice").hide();
+  $("#search").keyup((event) => {
+    switch(event.keyCode) {
+      case 13:
+      case 27:
+      case 38:
+      case 40:
+      break;
+
+      default:
+        $.ajax({
+          url: 'http://localhost:8000/search-advice/',
+          type: 'GET',
+          dataType: 'json',
+          data: $("#search").serialize()
+        })
+        .done(function(response) {
+          let products = response.products;
+          if (products) {
+            $(".search__advice").empty();
+            $(".search__advice").show();
+            for (let i in products) {
+              $(".search__advice").append(`<div class='advice__item' id=${i}>`+products[i].name+"</div>");
+            }
+          } else {
+            $(".search__advice").empty();
+            $(".search__advice").show();
+            $(".search__advice").append(`<div class='advice__not'>Результаты не найдены</div>`);
+          }
+        })
+        .fail(function() {
+          console.log("error");
+        })
+        .always(function() {
+          console.log("complete");
+        });
+    }
+  })
+
+  $(document).on('click', '.advice__item', e => {
+    console.log('тут');
+    $('#search').val($('.advice__item').text());
+  })
+
+  $(document).on('click', e => {
+    if (e.target.className != '.search__advice' && e.target.className != 'search__input') {
+      $('.search__advice').fadeOut(100);
+    }
+  })
+
+  $(document).on('click', '#search', e => {
+    if ($('.search__advice').children('.advice__item')) {
+      $('.search__advice').show();
+    }
+  })
+
+});
