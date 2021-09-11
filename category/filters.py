654d6e68a65e33d21ed015871fd87376eecfc074
Nikita <nikita.motlitch@yandex.ru>
2021-09-11 15:09:52 +0300

    Add django_filter to requirements, INSTALLED_APPS. Create ProductFilter.

---
+++ category/filters.py
@@ -0,0 +1,13 @@
+import django_filters
+
+from .models import Product, Characteristic
+
+
+class ProductFilter(django_filters.FilterSet):
+    price = django_filters.RangeFilter(field_name='price')
+    characteristics = django_filters.ModelMultipleChoiceFilter(
+        queryset=Characteristic.objects.none(), label='Характеристики')
+
+    class Meta:
+        model = Product
+        fields = ['rating']
--- shop/settings.py
+++ shop/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'django.contrib.humanize',
     'mptt',
     'category',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
