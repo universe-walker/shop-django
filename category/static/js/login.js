db833e7c7fa5dabbb156b5ad7d791190c86e0b51
Nikita <nikita.motlitch@yandex.ru>
2021-10-20 23:13:34 +0300

    Add scripts for login page

---
+++ category/static/js/login.js
@@ -0,0 +1,19 @@
+$(function() {
+    $('.login__btn').click(function () {
+        $('.login__form-signin').toggleClass('login__form-signin_left');
+        $('.login__form-signup').toggleClass('login__form-signup_left');
+        $('.login__frame').toggleClass('login__frame_long');
+        if(document.querySelector('.login__signin_active')) {
+            $(".login__signin_active").toggleClass("login__signin_active").toggleClass('login__signin_inactive');
+        } else {
+            $(".login__signin_inactive").toggleClass("login__signin_inactive").toggleClass("login__signin_active");
+        }
+        if(document.querySelector('.login__signup_active')) {
+            $(".login__signup_active").toggleClass("login__signup_active").toggleClass('login__signup_inactive');
+        } else {
+            $(".login__signup_inactive").toggleClass("login__signup_inactive").toggleClass("login__signup_active");
+        }
+    })
+
+    
+});
